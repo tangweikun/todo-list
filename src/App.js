23bfb4ceaf2d537d4d1f00c58d888f805a6a0c2a
tangweikun <819573105@qq.com>
2020-03-27 15:22:46 +0800

    :sparkles: add loading state

--- src/App.js
+++ src/App.js
@@ -18,7 +18,8 @@ const initialState = {
         .slice(6)
     }
   ],
-  doneIds: new Set()
+  doneIds: new Set(),
+  isLoading: false
 };
 
 function reducer(state, action) {
@@ -26,11 +27,16 @@ function reducer(state, action) {
 
   switch (type) {
     case "ADD":
-      return { ...state, todos: [...state.todos, payload.task] };
+      return {
+        ...state,
+        todos: [...state.todos, payload.task],
+        isLoading: false
+      };
     case "DELETE":
       state.doneIds.delete(payload.taskId);
       return {
         ...state,
+        isLoading: false,
         todos: state.todos.filter(x => x.id !== payload.taskId),
         doneIds: state.doneIds
       };
@@ -43,6 +49,8 @@ function reducer(state, action) {
     case "UNDO":
       state.doneIds.delete(payload.id);
       return { ...state, doneIds: state.doneIds };
+    case "LOADING":
+      return { ...state, isLoading: true };
     default:
       throw new Error();
   }
@@ -50,33 +58,41 @@ function reducer(state, action) {
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
-  const { todos, doneIds } = state;
+  const { todos, doneIds, isLoading } = state;
   const [newTask, setNewTask] = useState("");
 
   function _handleAdd() {
     if (newTask !== "") {
-      dispatch({
-        type: "ADD",
-        payload: {
-          task: {
-            title: newTask,
-            id: Math.random()
-              .toString(16)
-              .slice(6)
+      dispatch({ type: "LOADING" });
+
+      setTimeout(() => {
+        dispatch({
+          type: "ADD",
+          payload: {
+            task: {
+              title: newTask,
+              id: Math.random()
+                .toString(16)
+                .slice(6)
+            }
           }
-        }
-      });
-      setNewTask("");
+        });
+        setNewTask("");
+      }, 1000);
     }
   }
 
   function _handleDelete(taskId) {
-    dispatch({
-      type: "DELETE",
-      payload: {
-        taskId
-      }
-    });
+    dispatch({ type: "LOADING" });
+
+    setTimeout(() => {
+      dispatch({
+        type: "DELETE",
+        payload: {
+          taskId
+        }
+      });
+    }, 1000);
   }
 
   function _handleDone(id) {
@@ -101,49 +117,88 @@ function App() {
     setNewTask(e.target.value);
   }
 
-  console.log(todos, doneIds);
-
   return (
-    <div className="container">
-      <div className="title">Demo</div>
-      <div>
-        {todos.map(item => (
-          <div key={item.id} className="row">
-            <span
-              role="img"
-              aria-label="checked"
-              onClick={() =>
-                doneIds.has(item.id)
-                  ? _handleUndo(item.id)
-                  : _handleDone(item.id)
-              }
-            >
-              ✔️
-            </span>
-            <span
-              style={{
-                marginLeft: "6px",
-                color: doneIds.has(item.id) ? "red" : "green",
-                textDecoration: doneIds.has(item.id) ? "line-through" : "unset"
-              }}
-            >
-              {item.title}
-            </span>
-            <span style={{ flexGrow: 1 }}></span>
-            <span
-              onClick={() => _handleDelete(item.id)}
-              role="img"
-              aria-label="close"
-            >
-              ❌
+    <>
+      {isLoading && (
+        <div
+          style={{
+            position: "fixed",
+            opacity: 0.2,
+            height: "100%",
+            width: "100%"
+          }}
+        >
+          <div className="loading">
+            <span role="img" aria-label="close">
+              ❄️
             </span>
           </div>
-        ))}
-      </div>
+        </div>
+      )}
 
-      <input value={newTask} onChange={_handleChange} />
-      <div onClick={_handleAdd}>Add</div>
-    </div>
+      <div className="container">
+        <div className="title">Demo</div>
+
+        <div>
+          {todos.map(item => (
+            <div key={item.id} className="row">
+              <span
+                role="img"
+                aria-label="checked"
+                onClick={() =>
+                  doneIds.has(item.id)
+                    ? _handleUndo(item.id)
+                    : _handleDone(item.id)
+                }
+              >
+                ✔️
+              </span>
+              <span
+                style={{
+                  overflow: "hidden",
+                  textOverflow: "ellipsis",
+                  marginLeft: "6px",
+                  color: doneIds.has(item.id) ? "red" : "green",
+                  textDecoration: doneIds.has(item.id)
+                    ? "line-through"
+                    : "unset"
+                }}
+              >
+                {item.title}
+              </span>
+              <span style={{ flexGrow: 1 }}></span>
+              <span
+                onClick={() => _handleDelete(item.id)}
+                role="img"
+                aria-label="close"
+              >
+                ❌
+              </span>
+            </div>
+          ))}
+        </div>
+
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+          <input
+            value={newTask}
+            onChange={_handleChange}
+            style={{ padding: "4px" }}
+          />
+          <div
+            onClick={_handleAdd}
+            style={{
+              cursor: "pointer",
+              background: "grey",
+              color: "#fff",
+              border: "solid",
+              width: "100px"
+            }}
+          >
+            Add
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
